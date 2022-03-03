9695fe35f25e85ebd61127323dbf4721c2ea2fbb
snglbyrr8 <63016233+snglbyrr8@users.noreply.github.com>
2022-03-03 22:51:13 +0300

    Day1
    Day one exercises

---
+++ Day1/datatypes.js
@@ -0,0 +1,23 @@
+console.log(typeof 'name')
+console.log(typeof  5)
+console.log(typeof 5.2)
+console.log(typeof true)
+
+let one
+let two
+let three
+let four
+
+let name = 'Songul'
+let lastName = 'Bayer'
+let maritalStatus = 'Single'
+let country = 'Turkey'
+let age = 21
+
+let name_ = 'Songul',lastName_='Bayer', condition = 'single', country_='Turkey',age_=21
+
+let myAge=21
+let yourAge=35
+
+console.log(myAge)
+console.log(yourAge)
---
+++ Day1/variable.js
@@ -0,0 +1,13 @@
+//comments can make code readable
+//welcome to 30DaysOFJavaScript
+/*
+Comments can make code readable,
+easy to reuse and informative
+*/
+console.log('Hello World');
+console.log(typeof 'hello!');
+
+let string = 'string'
+let boolean = true
+let undefined
+let null_ = null
